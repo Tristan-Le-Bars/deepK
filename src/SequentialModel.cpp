22c245e83910c76d6387478a131eaa07881c808f
Tristan-Le-Bars <tristan.le-bars@epitech.eu>
2022-09-29 13:58:04 +0200

    [ADD] - adding the completion of neurones and synapses vectors in the AddLayer method from SequentialModel

--- src/SequentialModel.cpp
+++ src/SequentialModel.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <random>
 
 class SequentialModel
 {
 private:
     int layers_nbr;
-    std::vector<std::vector<int>> neural_matrix;
+    std::vector<std::vector<double>> neural_matrix;
+    std::vector<std::vector<double>> synaptic_matrix;
     std::vector<std::string> activation_functions_matrix;
 
 
@@ -14,25 +18,36 @@ public:
     ~SequentialModel();
 
     // Methods
-    AddLayer(int neurons_nbr, std::string activation_function);
+    // AddLayer(int neurons_nbr, std::string activation_function);
+
+    SequentialModel::SequentialModel(/* args */){}
+    SequentialModel::~SequentialModel(){}
+
+
+    // seters
+    void SetLayersNbr(int l){layers_nbr = l;};
+    void SetNeuralMatrix(std::vector<std::vector<double>> n){neural_matrix = n;};
+    void SetActivationFuntionMatrix(std::vector<std::string> a){activation_functions_matrix = a;};
+
+
+    // geters
+    int GetLayerNbr(){return layers_nbr;}
+    std::vector<std::vector<double>> GetNeuralMatrix(){return neural_matrix;}
+    std::vector<std::string> GetActivationFunctionMatrix(){return activation_functions_matrix;}
+
+    // methodes
+    void AddLayer(int neurons_nbr, std::string activation_function) {
+        std::vector<double> neurons(neurons_nbr, 0);
+        std::vector<double> synapses;
+
+        if(neural_matrix.size() >= 1){
+            for(int i; i < neural_matrix.back().size() * neurons_nbr; i++){
+                srand(time(0)); 
+                double r = rand() % 100;  
+                synapses.push_back(r);
+            }
+        }
+        neural_matrix.push_back(neurons);
+    }
+
 };
-
-SequentialModel::SequentialModel(/* args */){}
-SequentialModel::~SequentialModel(){}
-
-
-// seters
-void SetLayersNbr(int l){layer_nbr = l;};
-void SetNeuralMatrix(std::vector<std::vector<int>> n){neural_matrix = n;};
-void SetActivationFuntionMatrix(std::vector<std::string> a){activation_functions_matrix = a;};
-
-
-// geters
-int GetLayerNbr(){return layer_nbr};
-int GetNeuralMatrix(){return neural_matrix};
-int GetActivationFunctionMatrix(){return activationFunctionMatrix}
-
-// methodes
-SequentialModel::AddLayer(int neurons_nbr, std::string activation_function) {
-    neural_matrix.
-}
