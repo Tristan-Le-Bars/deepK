5920ff421ae382731c8a0a2bd0d3b1a8c0a436fb
Tristan-Le-Bars <tristan.le-bars@epitech.eu>
2022-09-29 15:45:14 +0200

    [ADD] - create three activation function methods

--- src/ActivationFunctions.cpp
+++ src/ActivationFunctions.cpp
@@ -1,11 +1,25 @@
-class ActivationFunctions
-{
-private:
-    /* data */
-public:
-    ActivationFunctions(/* args */);
-    ~ActivationFunctions();
+#include <iostream>
+
+class ActivationFunctions{
+    private:
+        std::string function;
+    public:
+        // Constructors
+        ActivationFunctions(/* args */);
+        ~ActivationFunctions();
+
+        // Seters
+        void SetFunction(std::string f){function = f;}
+
+        // Geters
+        std::string GetFunction(){return function;}
+
+        // Methods
+        double Logistic(){}
+        double ReLU(){}
+        double Tanh(){}
 };
 
 ActivationFunctions::ActivationFunctions(/* args */){}
 ActivationFunctions::~ActivationFunctions(){}
+
--- src/SequentialModel.cpp
+++ src/SequentialModel.cpp
@@ -3,50 +3,49 @@
 #include <string>
 #include <random>
 
-class SequentialModel
-{
-private:
-    int layers_nbr;
-    std::vector<std::vector<double>> neural_matrix;
-    std::vector<std::vector<double>> synaptic_matrix;
-    std::vector<std::string> activation_functions_matrix;
+class SequentialModel{
+    private:
+        int layers_nbr;
+        std::vector<std::vector<double>> neural_matrix;
+        std::vector<std::vector<double>> synaptic_matrix;
+        std::vector<std::string> activation_functions_matrix;
 
 
-public:
-    // Constructors
-    SequentialModel(/* args */);
-    ~SequentialModel();
+    public:
+        // Constructors
+        SequentialModel(/* args */);
+        ~SequentialModel();
 
-    // Methods
-    // AddLayer(int neurons_nbr, std::string activation_function);
-
-    SequentialModel::SequentialModel(/* args */){}
-    SequentialModel::~SequentialModel(){}
+        // Methods
+        // AddLayer(int neurons_nbr, std::string activation_function);
 
 
-    // seters
-    void SetLayersNbr(int l){layers_nbr = l;};
-    void SetNeuralMatrix(std::vector<std::vector<double>> n){neural_matrix = n;};
-    void SetActivationFuntionMatrix(std::vector<std::string> a){activation_functions_matrix = a;};
+        // seters
+        void SetLayersNbr(int l){layers_nbr = l;};
+        void SetNeuralMatrix(std::vector<std::vector<double>> n){neural_matrix = n;};
+        void SetActivationFuntionMatrix(std::vector<std::string> a){activation_functions_matrix = a;};
 
 
-    // geters
-    int GetLayerNbr(){return layers_nbr;}
-    std::vector<std::vector<double>> GetNeuralMatrix(){return neural_matrix;}
-    std::vector<std::string> GetActivationFunctionMatrix(){return activation_functions_matrix;}
+        // geters
+        int GetLayerNbr(){return layers_nbr;}
+        std::vector<std::vector<double>> GetNeuralMatrix(){return neural_matrix;}
+        std::vector<std::string> GetActivationFunctionMatrix(){return activation_functions_matrix;}
 
-    // methodes
-    void AddLayer(int neurons_nbr, std::string activation_function) {
-        std::vector<double> neurons(neurons_nbr, 0);
-        std::vector<double> synapses;
+        // methodes
+        void AddLayer(int neurons_nbr, std::string activation_function) {
+            std::vector<double> neurons(neurons_nbr, 0);
+            std::vector<double> synapses;
 
-        if(neural_matrix.size() >= 1){
-            for(int i; i < neural_matrix.back().size() * neurons_nbr; i++){
-                srand(time(0)); 
-                double r = rand() % 100;  
-                synapses.push_back(r);
+            if(neural_matrix.size() >= 1){
+                for(int i; i < neural_matrix.back().size() * neurons_nbr; i++){
+                    srand(time(0)); 
+                    double r = rand() % 100;  
+                    synapses.push_back(r);
+                }
             }
+            neural_matrix.push_back(neurons);
         }
-        neural_matrix.push_back(neurons);
-    }
 };
+
+SequentialModel::SequentialModel(/* args */){}
+SequentialModel::~SequentialModel(){}
