3bf9bb6affed11996c6a1409269f471b5ccafcba
Tristan-Le-Bars <tristan.le-bars@epitech.eu>
2022-10-02 22:45:41 +0200

    [ADD] - adding the Hinge method in the LossFunctions class

--- src/LossFunctions.cpp
+++ src/LossFunctions.cpp
@@ -20,6 +20,7 @@ public:
         for(int i; i < labels.size(); i++){
             loss.push_back((labels[i] - output_values[i]) * (labels[i] - output_values[i]));
         }
+        
         return loss;
     }
 
@@ -33,6 +34,25 @@ public:
             if(labels[i] == 0)
                 loss.push_back(-std::log10(1-output_values[i]));
         }
+
+        return loss;
+    }
+
+    std::vector<double> Hinge(std::vector<double> labels, std::vector<double> output_values){
+        std::vector<double> loss;
+        std::vector<double> buffer;
+
+        for(int i; i < labels.size(); i++){
+            if(labels[i] == 0)
+                labels[i] = -1;
+        }
+
+        for(int i; i < labels.size(); i++){
+            buffer = {0, 1 - labels[i] * output_values[i]};
+            loss.push_back(*std::max_element(buffer.begin(), buffer.end()));
+        }
+
+        return loss;
     }
 };
 
