ab909e2ac4b4521e65211572161f375780b4bfcc
Tristan-Le-Bars <tristan.le-bars@epitech.eu>
2022-10-02 21:04:11 +0200

    [ADD] - add the BinaryCrossEntropy method in the LossFunctions class

--- src/LossFunctions.cpp
+++ src/LossFunctions.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 class LossFunctions
 {
@@ -13,12 +14,25 @@ public:
     ~LossFunctions();
 
     // Public methods
-    std::vector<double> MeanSquaredError(std::vector<double> label, std::vector<double> output_values){
+    std::vector<double> MeanSquaredError(std::vector<double> labels, std::vector<double> output_values){
         std::vector<double> loss;
-        for(int i; i < label.size(); i++){
-            loss.push_back((label[i] - output_values[i]) * (label[i] - output_values[i]));
+
+        for(int i; i < labels.size(); i++){
+            loss.push_back((labels[i] - output_values[i]) * (labels[i] - output_values[i]));
         }
         return loss;
+    }
+
+    std::vector<double> BinaryCrossEntropy(std::vector<double> labels, std::vector<double> output_values){
+        /* Make sure the labels are 0 and 1, else, change them to 0 and 1*/
+        std::vector<double> loss;
+
+        for(int i; i < labels.size(); i++){
+            if(labels[i] == 1)
+                loss.push_back(-std::log10(output_values[i]));
+            if(labels[i] == 0)
+                loss.push_back(-std::log10(1-output_values[i]));
+        }
     }
 };
 
