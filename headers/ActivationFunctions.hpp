97dcf6bb1087c3e0c376f763963b3114d27917cf
Tristan-Le-Bars <tristan.le-bars@epitech.eu>
2022-11-23 10:19:59 +0100

    [FIX] - fix the ActivationFunctions files

--- headers/ActivationFunctions.hpp
+++ headers/ActivationFunctions.hpp
@@ -2,10 +2,10 @@
 #define CLASS_ACTIVATION_FUNCTIONS
 
 class ActivationFunctions {
-public:
-    double Logistic(double x);
-    double ReLU(double x);
-    double Tanh(double x);
+    public:
+        double Logistic(double x);
+        double ReLU(double x);
+        double Tanh(double x);
 };
 
 #endif CLASS_ACTIVATION_FUNCTIONS
--- src/ActivationFunctions.cpp
+++ src/ActivationFunctions.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <cmath>
+#include "../headers/ActivationFunctions.hpp"
 
-class ActivationFunctions{
-    private:
-    public:
-        // Constructor
-        ActivationFunctions(/* args */);
 
-        // Destructor
-        ~ActivationFunctions();
 
-        // Public methods
-        double Logistic(double x){return (1/(1 + exp(-x)));}
-        double ReLU(double x){return std::max(x, 0.0);}
-        double Tanh(double x){return std::tanh(x);}
-};
-
+// Constructor
 ActivationFunctions::ActivationFunctions(/* args */){}
+
+// Destructor
 ActivationFunctions::~ActivationFunctions(){}
 
+// Methods
+double ActivationFunctions::Logistic(double x){return (1/(1 + exp(-x)));}
+double ActivationFunctions::ReLU(double x){return std::max(x, 0.0);}
+double ActivationFunctions::Tanh(double x){return std::tanh(x);}
