3343a5afe784b0089eadf4b7e7964257abb63ec1
Tristan-Le-Bars <tristan.le-bars@epitech.eu>
2023-01-07 23:48:48 +0100

    [FIX] - fix the MeanSquaredError function

--- src/LossFunctions.cpp
+++ src/LossFunctions.cpp
@@ -11,21 +11,23 @@ LossFunctions::LossFunctions(/* args */){}
 LossFunctions::~LossFunctions(){}
 
 // Methods
-std::vector<double> LossFunctions::MeanSquaredError(std::vector<double> labels, std::vector<double> output_values){
-    std::vector<double> loss;
+void LossFunctions::MeanSquaredError(std::vector<double> labels, std::vector<double> output_values, std::vector<double> *loss){
+    std::vector<double> loss_buffer;
 
-    for(int i; i < labels.size(); i++){
-        loss.push_back((labels[i] - output_values[i]) * (labels[i] - output_values[i]));
+    std::cout << "output values size = " << output_values.size() << std::endl;
+
+    for(int i = 0; i < output_values.size(); i++){
+        loss_buffer.push_back((labels[i] - output_values[i]) * (labels[i] - output_values[i]));
     }
-    
-    return loss;
+
+    *loss = loss_buffer;
 }
 
 std::vector<double> LossFunctions::BinaryCrossEntropy(std::vector<double> labels, std::vector<double> output_values){
     /* Make sure the labels are 0 and 1, else, change them to 0 and 1*/
     std::vector<double> loss;
 
-    for(int i; i < labels.size(); i++){
+    for(int i = 0; i < labels.size(); i++){
         if(labels[i] == 1)
             loss.push_back(-std::log10(output_values[i]));
         if(labels[i] == 0)
@@ -39,12 +41,12 @@ std::vector<double> LossFunctions::Hinge(std::vector<double> labels, std::vector
     std::vector<double> loss;
     std::vector<double> buffer;
 
-    for(int i; i < labels.size(); i++){
+    for(int i = 0; i < labels.size(); i++){
         if(labels[i] == 0)
             labels[i] = -1;
     }
 
-    for(int i; i < labels.size(); i++){
+    for(int i = 0; i < labels.size(); i++){
         buffer = {0, 1 - labels[i] * output_values[i]};
         loss.push_back(*std::max_element(buffer.begin(), buffer.end()));
     }
